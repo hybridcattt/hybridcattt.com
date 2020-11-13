f1134a06edb72fb16ab4cd7826e39a38c7aaee88
Marina Gornostaeva <gornostaevamv@gmail.com>
2020-11-13 18:13:26 +0100

    Add sitemap

---
+++ scripts/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ -d splash ]; then 
+  echo Splash is already checked out
+else
+  git clone https://github.com/johnsundell/splash.git
+fi
+
+cd splash
+swift build -c release
+install .build/release/SplashHTMLGen ../SplashHTMLGen
+
+cd ..
+jekyll serve
